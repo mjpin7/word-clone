261f838a48f82fcd295b0b3b8827f8d1e4587587
MJ Pinheiro <mj.pinheiro@agilebits.com>
2023-05-10 13:49:49 -0400

    chore: exercise 3

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { range } from "../../utils";
+import Guess from "../Guess/Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function Guesses({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => (
-        <p className="guess" key={index}>{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
+        <Guess key={index} guess={guesses[index]} />
       ))}
     </div>
   );
